5548d8a74aafe8b15d9b5c73d9e0ea13dcd895a0
milad <baranimilad70@yahoo.com>
2024-01-16 19:17:46 +0330

    #5-Adding User class

---
+++ src/main/java/model/User.java
@@ -0,0 +1,77 @@
+package model;
+
+public class User {
+
+    int id;
+    String name;
+    String phoneNumber;
+    int nationalCode;
+
+
+    //constructor
+
+    public User(){
+    }
+
+    public User(int id, String name, String phoneNumber, int nationalCode) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    public User(String name, String phoneNumber, int nationalCode) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    //get and set
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(int nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+
+    //method
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", nationalCode=" + nationalCode +
+                '}';
+    }
+}
