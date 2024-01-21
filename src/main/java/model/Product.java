8eba9fc3afeedcd9105d4aa0c499983c8287a447
milad <baranimilad70@yahoo.com>
2024-01-21 15:52:08 +0330

    #22-Adding Shareholder_BrandRepository

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,89 +1,29 @@
 package model;
 
+import lombok.*;
+
 import java.util.Arrays;
 import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 
 public class Product {
 
     private int id;
     private String name;
     private Date createDate;
-    private Category[] category;
-    private Brand[] brand;
+    private int categoryId;
+    private int brandId;
 
-    //constructor
-
-    public Product(){}
-
-    public Product(int id, String name, Date createDate, Category[] category, Brand[] brand) {
-        this.id = id;
+    //CONSTRUCTOR
+    public Product(String name, Date createDate, int categoryId, int brandId) {
         this.name = name;
         this.createDate = createDate;
-        this.category = category;
-        this.brand = brand;
-    }
-
-    public Product(String name, Date createDate, Category[] category, Brand[] brand) {
-        this.name = name;
-        this.createDate = createDate;
-        this.category = category;
-        this.brand = brand;
-    }
-
-    //get and set
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Category[] getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category[] category) {
-        this.category = category;
-    }
-
-    public Brand[] getBrand() {
-        return brand;
-    }
-
-    public void setBrand(Brand[] brand) {
-        this.brand = brand;
-    }
-
-    //method
-
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", createDate=" + createDate +
-                ", category=" + Arrays.toString(category) +
-                ", brand=" + Arrays.toString(brand) +
-                '}';
+        this.categoryId = categoryId;
+        this.brandId = brandId;
     }
 }
---
+++ src/main/java/repository/Shareholder_BrandRepository.java
@@ -0,0 +1,96 @@
+package repository;
+
+import model.Product;
+import model.Shareholder_Brand;
+
+import java.sql.*;
+
+public class Shareholder_BrandRepository {
+private final Connection connection;
+    public Shareholder_BrandRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean createSh_Br(Shareholder_Brand shareholderBrand) throws SQLException {
+
+        String sql = "insert into shareholder_brand(shareholder_id_fk, brand_id_fk) vlaues(?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setInt(1, shareholderBrand.getShareholder_id());
+        preparedStatement.setInt(2, shareholderBrand.getBrand_id());
+
+        int result = preparedStatement.executeUpdate();
+        return (result == 1) ? true : false;
+    }
+
+    public void readShareholdersBrand(int shareholder_id) throws SQLException {
+        String sql ="select shareholder_id_fk,shareholder.name,brand_id_fk, b.name\n" +
+                "from shareholder\n" +
+                "         inner join shareholder_brand sb on shareholder.id = sb.shareholder_id_fk\n" +
+                "         inner join brand b on b.id = sb.brand_id_fk\n" +
+                "GROUP BY shareholder.id, shareholder.name, phone_number, national_code, shareholder_id_fk, brand_id_fk, b.id, b.name,\n" +
+                "         website, description\n" +
+                "having shareholder.id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, shareholder_id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            int shareId = resultSet.getInt(1);
+            String share_name = resultSet.getString(2);
+            int brandId = resultSet.getInt(3);
+            String brand_name = resultSet.getString(4);
+            System.out.printf("\nid : %s ->shareholder_name : %s  | id :%s -> brand : %s",shareId,share_name,brandId,brand_name);
+        }
+    }
+
+
+    public void readBrandsShareholder(int brand_id) throws SQLException {
+        String sql = "select brand_id_fk, brand.name,shareholder_id_fk, s.name\n" +
+                "from brand\n" +
+                "         inner join shareholder_brand sb on brand.id = sb.brand_id_fk\n" +
+                "         join shareholder s on s.id = sb.shareholder_id_fk\n" +
+                "group by brand_id_fk, brand.name, shareholder_id_fk, s.name\n" +
+                "HAVING brand_id_fk = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, brand_id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            int brandId = resultSet.getInt(1);
+            String share_name = resultSet.getString(2);
+            int shareId = resultSet.getInt(3);
+            String brand_name = resultSet.getString(4);
+            System.out.printf("\nid : %s -> brand : %s  | id : %s ->  shareholder : %s",brandId,brand_name,shareId,share_name);
+        }
+    }
+
+
+    public boolean updateShare_Brand(Shareholder_Brand setShareholderBrand1 , Shareholder_Brand shareholderBrand2) throws SQLException {
+        String sql = "update shareholder_brand\n" +
+                "set shareholder_id_fk = ? , brand_id_fk = ?\n" +
+                "where shareholder_id_fk = ? and brand_id_fk = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, setShareholderBrand1.getShareholder_id());
+        preparedStatement.setInt(2, setShareholderBrand1.getBrand_id());
+        preparedStatement.setInt(3, shareholderBrand2.getShareholder_id());
+        preparedStatement.setInt(4, shareholderBrand2.getBrand_id());
+
+        int result = preparedStatement.executeUpdate();
+        return (result == 1) ? true : false;
+    }
+
+    public boolean dropProduct(Shareholder_Brand shareholderBrand) throws SQLException {
+        String sql = "delete from shareholder_brand \n" +
+                "where shareholder_id_fk = ? and brand_id_fk = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, shareholderBrand.getShareholder_id());
+        preparedStatement.setInt(1, shareholderBrand.getBrand_id());
+
+        int resutl = preparedStatement.executeUpdate();
+        return (resutl == 1) ? true : false;
+    }
+
+}
